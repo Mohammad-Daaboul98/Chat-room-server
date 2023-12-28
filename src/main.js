7e96746c8801dafa74ab6601d21599c41780289f
unknown <mohammad.daaboul.99@gmail.com>
2023-12-28 14:05:42 +0300

    test

--- src/main.js
+++ src/main.js
@@ -21,17 +21,17 @@ export default async ({ req, res, log, error }) => {
     //   ID.unique(),
     //   room_name
     // );
-    log(req.bodyRaw);                     // Raw request body, contains request data
-    log(JSON.stringify(req.body));        // Object from parsed JSON request body, otherwise string
-    log(JSON.stringify(req.headers));     // String key-value pairs of all request headers, keys are lowercase
-    log(req.scheme);                      // Value of the x-forwarded-proto header, usually http or https
-    log(req.method);                      // Request method, such as GET, POST, PUT, DELETE, PATCH, etc.
-    log(req.url);                         // Full URL, for example: http://awesome.appwrite.io:8000/v1/hooks?limit=12&offset=50
-    log(req.host);                        // Hostname from the host header, such as awesome.appwrite.io
-    log(req.port);                        // Port from the host header, for example 8000
-    log(req.path);                        // Path part of URL, for example /v1/hooks
-    log(req.queryString);                 // Raw query params string. For example "limit=12&offset=50"
-    log(JSON.stringify(req.query));       // Parsed query params. For example, req.query.limit
+    console.log(req.bodyRaw);                     // Raw request body, contains request data
+    console.log(JSON.stringify(req.body));        // Object from parsed JSON request body, otherwise string
+    console.log(JSON.stringify(req.headers));     // String key-value pairs of all request headers, keys are lowercase
+    console.log(req.scheme);                      // Value of the x-forwarded-proto header, usually http or https
+    console.log(req.method);                      // Request method, such as GET, POST, PUT, DELETE, PATCH, etc.
+    console.log(req.url);                         // Full URL, for example: http://awesome.appwrite.io:8000/v1/hooks?limit=12&offset=50
+    console.log(req.host);                        // Hostname from the host header, such as awesome.appwrite.io
+    console.log(req.port);                        // Port from the host header, for example 8000
+    console.log(req.path);                        // Path part of URL, for example /v1/hooks
+    console.log(req.queryString);                 // Raw query params string. For example "limit=12&offset=50"
+    console.log(JSON.stringify(req.query));       // Parsed query params. For example, req.query.limit
 
     return res.send("All the request parameters are logged to the Appwrite Console.");
   }
