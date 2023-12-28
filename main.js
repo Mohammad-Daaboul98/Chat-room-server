27e16d542b60123993f2ebb56d71233d35c8738c
unknown <mohammad.daaboul.99@gmail.com>
2023-12-28 12:37:18 +0300

    first commit

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-const sdk = require('node-appwrite');
-const API_ENDPOINT = process.env.API_ENDPOINT
-const PROJECT_ID = process.env.PROJECT_ID
-const DATABASE_ID = process.env.DATABASE_ID
-const API_KEY = process.env.API_KEY
+const API_ENDPOINT = process.env.API_ENDPOINT;
+const PROJECT_ID = process.env.PROJECT_ID;
+const DATABASE_ID = process.env.DATABASE_ID;
+const API_KEY = process.env.API_KEY;
 
 export default async ({ req, res, log, error }) => {
+  const sdk = require("node-appwrite");
 
-    // Init SDK
-    const client = new sdk.Client();
-    
-    const databases = new sdk.Databases(client);
-    
-    client
-        .setEndpoint(API_ENDPOINT) 
-        .setProject(PROJECT_ID) 
-        .setKey(API_KEY) 
-    ;
-    
-    if(req.method == 'GET'){
-    const promise = databases.createCollection(DATABASE_ID,sdk.ID.unique(),'privateRoom');
-        promise.then(function (response) {
-            log(response);
-            res.send('hi')
-        }, function (error) {
-            log(error);
-        });
-    }
+  // Init SDK
+  const client = new sdk.Client();
 
-    
-}
+  const databases = new sdk.Databases(client);
+
+  client.setEndpoint(API_ENDPOINT).setProject(PROJECT_ID).setKey(API_KEY);
+
+  if (req.method == "GET") {
+    const promise = databases.createCollection(
+      DATABASE_ID,
+      sdk.ID.unique(),
+      "privateRoom"
+    );
+    promise.then(
+      function (response) {
+        log(response);
+        res.send("hi");
+      },
+      function (error) {
+        log(error);
+      }
+    );
+  }
+};
