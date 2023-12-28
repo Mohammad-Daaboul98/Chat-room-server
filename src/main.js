fc8222b4cd5735f0214897184c8ca0244a906b18
unknown <mohammad.daaboul.99@gmail.com>
2023-12-29 01:42:06 +0300

    test

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ export default async ({ req, res, log, error }) => {
     //   ID.unique(),
     //   room_name
     // );
-    const formData = JSON.querystring(req.body);
+    const formData = querystring(req.body);
 
     log(formData);
     log(req.body);
