f57a691dbd8c9a2ed0878cc54f45fb4adc7cf017
unknown <mohammad.daaboul.99@gmail.com>
2023-12-28 12:43:31 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
+import { Client ,Databases } from "node-appwrite";
 const API_ENDPOINT = process.env.API_ENDPOINT;
 const PROJECT_ID = process.env.PROJECT_ID;
 const DATABASE_ID = process.env.DATABASE_ID;
 const API_KEY = process.env.API_KEY;
 
 export default async ({ req, res, log, error }) => {
-  const sdk = require("node-appwrite");
+  // const sdk = require("node-appwrite");
 
   // Init SDK
-  const client = new sdk.Client();
+  const client = new Client();
 
-  const databases = new sdk.Databases(client);
+  const databases = new Databases(client);
 
-  client.setEndpoint(API_ENDPOINT).setProject(PROJECT_ID).setKey(API_KEY);
+  client
+  .setEndpoint(API_ENDPOINT)
+  .setProject(PROJECT_ID)
+  .setKey(API_KEY);
 
   if (req.method == "GET") {
     const promise = databases.createCollection(
