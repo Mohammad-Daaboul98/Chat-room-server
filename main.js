95c5cd88f6a68693f4e33d7c395bfcc56222f191
unknown <mohammad.daaboul.99@gmail.com>
2023-12-28 12:28:06 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const sdk = require('node-appwrite');
+
+const API_ENDPOINT =process.env.API_ENDPOINT
+const PROJECT_ID =process.env.PROJECT_ID
+const DATABASE_ID =process.env.DATABASE_ID
+const API_KEY =process.env.API_KEY
+
+export default async ({ req, res, log, error }) => {
+
+    // Init SDK
+    const client = new sdk.Client();
+    
+    const databases = new sdk.Databases(client);
+    
+    client
+        .setEndpoint(API_ENDPOINT) 
+        .setProject(PROJECT_ID) 
+        .setKey(API_KEY) 
+    ;
+    
+    const promise = databases.createCollection(DATABASE_ID,sdk.ID.unique(),'privateRoom');
+    if(req.method == 'GET'){
+        promise.then(function (response) {
+            log(response);
+            res.send(response)
+        }, function (error) {
+            log(error);
+        });
+    }
+
+    
+}
--- src/main.js
+++
@@ -1,3 +0,0 @@
-export default async ({ req, res, log, error }) => {
-    return res.send('hello world')
-}
